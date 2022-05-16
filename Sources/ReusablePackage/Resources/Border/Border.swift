b6c6ae4f1f935d26000efd3ae192eb665c2fa414
Minata P Mahaarta <minataputra@gmail.com>
2022-05-16 13:13:56 +0700

    add border section

---
+++ Sources/ReusablePackage/Resources/Border/Border.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Minata Putra Mahaarta on 5/16/22.
+//
+
+import UIKit
+
+public class Border: UIView {
+    
+    func normalBorder(_ color: CGColor) {
+        layer.borderColor = color
+        layer.borderWidth = 1
+    }
+    
+}
