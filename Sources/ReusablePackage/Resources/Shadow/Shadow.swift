ec3228726c45e44b8818c5e7b9c986786fc12119
Minata P Mahaarta <minataputra@gmail.com>
2022-05-16 12:23:58 +0700

    remove _ view on shadow

--- Sources/ReusablePackage/Resources/Shadow/Shadow.swift
+++ Sources/ReusablePackage/Resources/Shadow/Shadow.swift
@@ -9,7 +9,7 @@ import UIKit
 
 public extension UIView {
     
-    func hardShadow(_ view: UIView) {
+    func hardShadow() {
         view.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
         view.layer.shadowRadius = 8.0
         view.layer.shadowOffset = CGSize(width: 0, height: 8)
