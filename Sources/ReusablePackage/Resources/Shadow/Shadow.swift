ae3055a5950e9a20d1da58772f90d3636be2465a
Minata P Mahaarta <minataputra@gmail.com>
2022-05-16 12:25:27 +0700

    remove view on shadow extension

--- Sources/ReusablePackage/Resources/Shadow/Shadow.swift
+++ Sources/ReusablePackage/Resources/Shadow/Shadow.swift
@@ -10,10 +10,10 @@ import UIKit
 public extension UIView {
     
     func hardShadow() {
-        view.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
-        view.layer.shadowRadius = 8.0
-        view.layer.shadowOffset = CGSize(width: 0, height: 8)
-        view.layer.shadowOpacity = 0.1
+        layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
+        layer.shadowRadius = 8.0
+        layer.shadowOffset = CGSize(width: 0, height: 8)
+        layer.shadowOpacity = 0.1
     }
     
 }
