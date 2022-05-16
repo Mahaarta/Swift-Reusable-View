3ebe430121b1d011f8c4ab0f15a958ef5970993e
Minata P Mahaarta <minataputra@gmail.com>
2022-05-16 12:06:34 +0700

    change shadow class to extension uiview

--- Sources/ReusablePackage/Resources/Shadow/Shadow.swift
+++ Sources/ReusablePackage/Resources/Shadow/Shadow.swift
@@ -7,12 +7,12 @@
 
 import UIKit
 
-public class shadow: UIView {
+public extension UIView {
     
-    public func hardShadow(_ view: UIView) {
-        view.layer.shadowColor = .black
+    func hardShadow(_ view: UIView) {
+        view.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
         view.layer.shadowRadius = 8.0
-        view.layer.shadowOffset = 8
+        view.layer.shadowOffset = CGSize(width: 0, height: 8)
         view.layer.shadowOpacity = 0.1
     }
     
