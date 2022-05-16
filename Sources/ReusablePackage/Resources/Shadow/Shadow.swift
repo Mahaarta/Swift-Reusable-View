2eeee5661836ffbb6cd8c4fbb245e498bccc169a
Minata P Mahaarta <minataputra@gmail.com>
2022-05-16 12:46:41 +0700

    add more shadow

--- Sources/ReusablePackage/Resources/Shadow/Shadow.swift
+++ Sources/ReusablePackage/Resources/Shadow/Shadow.swift
@@ -11,16 +11,37 @@ public extension UIView {
     
     func hardShadow() {
         layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
-        layer.shadowRadius = 8.0
+        layer.shadowRadius = 10.0
         layer.shadowOffset = CGSize(width: 0, height: 8)
         layer.shadowOpacity = 0.1
     }
     
     func mediumShadow() {
         layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
-        layer.shadowRadius = 16.0
-        layer.shadowOffset = CGSize(width: 0, height: 8)
+        layer.shadowRadius = 20.0
+        layer.shadowOffset = CGSize(width: 0, height: 16)
         layer.shadowOpacity = 0.1
+    }
+    
+    func softShadow() {
+        layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
+        layer.shadowRadius = 8.0
+        layer.shadowOffset = CGSize(width: 0, height: 2)
+        layer.shadowOpacity = 0.1
+    }
+    
+    func leftShadow() {
+        layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
+        layer.shadowRadius = 10.0
+        layer.shadowOffset = CGSize(width: -7, height: 0)
+        layer.shadowOpacity = 0.05
+    }
+    
+    func rightShadow() {
+        layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
+        layer.shadowRadius = 10.0
+        layer.shadowOffset = CGSize(width: 7, height: 0)
+        layer.shadowOpacity = 0.05
     }
     
 }
